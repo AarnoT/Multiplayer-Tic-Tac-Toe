44a5d2005ebbdfc569cb0790dd9183c1c16fed5c
AarnoT <aarnot.toiviainen@gmail.com>
2017-02-19 19:00:45 +0200

    Add game server

---
+++ server.py
@@ -0,0 +1,66 @@
+"""Tic-tac-toe game server."""
+
+import http.server
+
+
+class GameMatch:
+    """Store the state of a single game match."""
+
+    def __init__(self, player_1, board_size=3):
+        """Set instance variables."""
+        self.player_1 = player_1
+        self.player_2 = None
+        self.board = ['*' * board_size]  * board_size
+
+    def check_game_over(self):
+        """Check if the match has ended."""
+
+    def check_valid_move(self, move):
+        """Check if a move is valid."""
+
+
+class GameHandler(http.server.BaseHTTPRequestHandler):
+    """Handle requests and the game state."""
+
+    def __init__(self):
+        """Set instance variables."""
+        super(GameHandler, self).__init__()
+        self.paths = {'/' : self.index_page,
+                      '/create_game' : self.create_game,
+                      '/join_game' : self.join_game,
+                      '/game_state' : self.game_state,
+                      '/make_move' : self.make_move}
+        self.matches = list()
+
+    def index_page(self):
+        """Send the index page as a response."""
+
+    def create_game(self):
+        """Create a new match and send a response with the match id."""
+
+    def join_game(self):
+        """
+        Add a player to an existing match if there is a free slot.
+
+        Send a response that tells if it was succesful or not.
+        """
+
+    def game_state(self):
+        """Send the state of a specific match as a response."""
+
+    def make_move(self):
+        """Change the state of a specific match."""
+
+    def do_GET(self):
+        """Handle a GET request."""
+        self.send_response(200)
+        self.end_headers()
+        if self.path in self.paths:
+            self.paths[self.path]()
+
+
+def run_server():
+    """Handle requests in a loop."""
+    http_server = http.server.HTTPServer(('127.0.0.1', 8080), GameHandler)
+    while True:
+        http_server.handle_request()
